cd913479e387ba2e5be91ea2a51527d240d92d38
Aayush <aayushrawal98@gmail.com>
2020-08-05 08:17:18 -0500

    add libuvc

---
+++ libuvc/build/CMakeFiles/uvc.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sensor/Desktop/libuvc/src/ctrl-gen.c" "/home/sensor/Desktop/libuvc/build/CMakeFiles/uvc.dir/src/ctrl-gen.c.o"
+  "/home/sensor/Desktop/libuvc/src/ctrl.c" "/home/sensor/Desktop/libuvc/build/CMakeFiles/uvc.dir/src/ctrl.c.o"
+  "/home/sensor/Desktop/libuvc/src/device.c" "/home/sensor/Desktop/libuvc/build/CMakeFiles/uvc.dir/src/device.c.o"
+  "/home/sensor/Desktop/libuvc/src/diag.c" "/home/sensor/Desktop/libuvc/build/CMakeFiles/uvc.dir/src/diag.c.o"
+  "/home/sensor/Desktop/libuvc/src/frame.c" "/home/sensor/Desktop/libuvc/build/CMakeFiles/uvc.dir/src/frame.c.o"
+  "/home/sensor/Desktop/libuvc/src/init.c" "/home/sensor/Desktop/libuvc/build/CMakeFiles/uvc.dir/src/init.c.o"
+  "/home/sensor/Desktop/libuvc/src/misc.c" "/home/sensor/Desktop/libuvc/build/CMakeFiles/uvc.dir/src/misc.c.o"
+  "/home/sensor/Desktop/libuvc/src/stream.c" "/home/sensor/Desktop/libuvc/build/CMakeFiles/uvc.dir/src/stream.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "uvc_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
